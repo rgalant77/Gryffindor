2bb8b0b877fbde2b26b9e8ef3a977fd4ce29240d
Dario <galant233@gmail.com>
2024-11-30 20:26:47 -0300

    primer commit

---
+++ gryffindor/App/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def inicio(request):
+    return HttpResponse("Vista inicio")
+
+def cursos(request):
+    return HttpResponse("Vista cursos")
+
+def profesores(request):
+    return HttpResponse("Vista profesores")
+
+def estudiantes(request):
+    return HttpResponse("Vista estudiantes")
+
+def entregables(request):
+    return HttpResponse("Vista entregables")
